23a6698090eed3e9901802702637ce141ef2eb64
Your Name <you@example.com>
2017-12-15 16:55:43 -0800

    work on css exercise and linked sets exercise

---
+++ skeleton2/lib/p03_hash_set.rb
@@ -0,0 +1,55 @@
+require_relative 'p02_hashing'
+
+class HashSet
+  attr_reader :count
+
+  def initialize(num_buckets = 8)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(key)
+    resize! unless count < num_buckets
+    unless include?(key)
+      self[key] << key
+      @count+=1
+      return true
+    end
+    false
+  end
+
+  def include?(key)
+    self[key].include?(key)
+  end
+
+  def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[(num.hash % num_buckets)]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    copy_array = @store
+    @store = Array.new(num_buckets*2) { Array.new}
+    @count = 0
+
+    copy_array.each do |bucket|
+      bucket.each do |item|
+        insert(item)
+      end
+    end
+
+  end
+end
---
+++ skeleton2/lib/p04_linked_list.rb
@@ -0,0 +1,73 @@
+class Node
+  attr_accessor :key, :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    # optional but useful, connects previous link to next link
+    # and removes self from list.
+  end
+end
+
+class LinkedList
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @tail.next = @head
+    @head.prev = @tail
+  end
+
+  def [](i)
+    each_with_index { |link, j| return link if i == j }
+    nil
+  end
+
+  def first
+    @head.prev
+  end
+
+  def last
+    @tail.next
+  end
+
+  def empty?
+    return true if @head.prev == @tail
+  end
+
+  def get(key)
+  end
+
+  def include?(key)
+  end
+
+  def append(key, val)
+    key = Node.new(key, val)
+    key.prev = first
+    key.next = @head
+    @head.prev.next = key
+    @head.prev = key
+  end
+
+  def update(key, val)
+  end
+
+  def remove(key)
+  end
+
+  def each
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  # def to_s
+  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  # end
+end
